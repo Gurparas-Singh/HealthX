5dd90fef303fe0e55fa9d105b9112edc49f6cbb3
Gurparas Singh <78079839+Gurparas-Singh@users.noreply.github.com>
2023-08-13 09:05:16 +0530

    Update sketch_dht.ino

--- sketch_dht.ino
+++ sketch_dht.ino
@@ -20,18 +20,21 @@ DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 // Initialize Bluetooth
-SoftwareSerial bt(HC05_RX, HC05_TX);
+SoftwareSerial bt(8, 9);
 void setup() {
   Serial.begin(9600);
   bt.begin(9600);
-
-  lcd.begin(16, 2);
   
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
   dht.begin();
 }
 void loop() {
   int temperature = dht.readTemperature();
   int humidity = dht.readHumidity();
+
+  digitalWrite(10, HIGH);
 
     if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -43,7 +46,6 @@ void loop() {
     Serial.println("°C");
   }
 
-if (!isnan(temperature) && !isnan(humidity)) {
     // Display temperature on LCD
     lcd.setCursor(0, 0);
   lcd.print("Temperature:");
@@ -55,11 +57,18 @@ if (!isnan(temperature) && !isnan(humidity)) {
     lcd.setCursor(10, 1);
     lcd.print(22);
     lcd.print("%");
-  
+
 if(humidity>60){
   bt.print("Alert! High Humidity: ");
   bt.print(humidity);
   bt.print("%\n");
+
+  digitalWrite(10, HIGH);
+  tone(11, 100);
+  delay(200);
+  digitalWrite(10, LOW);
+  noTone(11); 
+  delay(50);
   }
   else{
     // Send data to Bluetooth
@@ -68,10 +77,14 @@ if(humidity>60){
     bt.print("%\n");
   }
 
-  if(temperature>30 || temperature<20){
+  if(temperature>35 ){
   bt.print("Alert! Extereme Temperature: ");
   bt.print(temperature);
   bt.print(" C\n");
+  tone(11, 1000); // Send 1KHz sound signal...
+  delay(500);        // ...for 1 sec
+  noTone(11);     // Stop sound...
+  delay(1000);        // ...for 1sec
   }
   else{
     // Send data to Bluetooth
@@ -80,14 +93,7 @@ if(humidity>60){
     bt.print(" C");
   }
 
-  } else {
-    // Display error message on LCD
-    lcd.setCursor(0, 0);
-    lcd.print("Sensor Error");
-    lcd.setCursor(0, 1);
-    lcd.print("Check Wiring");
-    bt.print("Failed");
-  }
+
 
   delay(2000); // Delay between readings
   lcd.clear();
